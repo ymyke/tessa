5679b6948bdd09046455c89bc741672e60532fc7
ymyke <michael.naef@gmail.com>
2022-04-28 22:32:21 +0200

    Make freezeargs work with lists as well

--- tessa/freezeargs.py
+++ tessa/freezeargs.py
@@ -3,6 +3,16 @@
 import functools
 from frozendict import frozendict
 
+
+def freeze(x: object) -> object:
+    """Freeze object."""
+    if isinstance(x, dict):
+        return frozendict(x)
+    if isinstance(x, list):
+        return tuple(x)
+    return x
+
+
 def freezeargs(func: callable) -> callable:
     """Transform mutable dictionary into immutable useful to be compatible with cache.
 
@@ -13,14 +23,10 @@ def freezeargs(func: callable) -> callable:
     @functools.wraps(func)
     def wrapped(*args, **kwargs):
         return func(
-            *[frozendict(x) if isinstance(x, dict) else x for x in args],
-            **{
-                k: frozendict(v) if isinstance(v, dict) else v
-                for k, v in kwargs.items()
-            },
+            *[freeze(x) for x in args],
+            **{k: freeze(v) for k, v in kwargs.items()},
         )
 
     wrapped.cache_info = func.cache_info
     wrapped.cache_clear = func.cache_clear
     return wrapped
-
