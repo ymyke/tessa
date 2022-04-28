964725f84f1b971b3100da3343ed84b8d04b00df
ymyke <michael.naef@gmail.com>
2022-04-28 22:53:28 +0200

    Simplify and remove obsolete code

--- tessa/rate_limiter.py
+++ tessa/rate_limiter.py
@@ -10,12 +10,12 @@ import time
 import pendulum
 
 guards = {
-    # Additional attributes that will be added dynamically are: last_call, wait_seconds
+    # `setup_guards` will add `last_call` to each guard.
     "investing": {
-        "initial_wait_seconds": 2,
+        "wait_seconds": 2,
     },
     "crypto": {
-        "initial_wait_seconds": 1,
+        "wait_seconds": 1,
     },
 }
 
@@ -24,9 +24,6 @@ def setup_guards() -> None:
     """Set up guards."""
     for guard in guards.values():
         guard["last_call"] = pendulum.parse("1900")
-        guard["wait_seconds"] = guard["initial_wait_seconds"]
-        # FIXME Can get wird of `initial_wait_seconds` if we don't end up implementing
-        # the exponential back-off at some point.
 
 
 # FIXME Turn rate_limit into a decorator that takes the type as an argument? cf
@@ -43,22 +40,6 @@ def rate_limit(type_: str) -> None:
     if diff < guard["wait_seconds"]:
         time.sleep(guard["wait_seconds"] - diff)
     guard["last_call"] = pendulum.now()
-
-    # FIXME The above is a very simple approach. We could also imagine having a variant
-    # that gets called with the retrieval function to be called and all the arguments
-    # and then also tests for a 429 error as shown below. In this case, we could get rid
-    # of the `type_` parameter and use the function name as the key to the guard dict.
-
-    # # Call the original function and return, increase wait_seconds exponentially if
-    # # a 429 error is encountered:
-    # try:
-    #     res = func(*args, **kwargs)
-    # except (requests.HTTPError, ConnectionError) as exc:
-    #     if "429" in exc:
-    #         guard["wait_seconds"] *= 2
-    #     raise exc
-
-    # guard["wait_seconds"] = guard["initial_wait_seconds"]  # Reset if no error
 
 
 setup_guards()
