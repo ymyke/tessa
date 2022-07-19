47e224b1c21ac3729937a4bb8eef0ee9a0eb0a98
ymyke <michael.naef@gmail.com>
2022-07-19 09:35:46 +0200

    Add some first tests

---
+++ tests/symbols/test_symbol.py
@@ -0,0 +1,24 @@
+"""Symbol-related tests."""
+
+from tessa.symbols.symbol import Symbol  # FIXME Better import?
+
+# pylint: disable=no-member,missing-function-docstring
+
+# FIXME Also check the tests in fignal around tickerconfig and Asset
+
+# FIXME Add more tests
+
+
+def test_initiator():
+    s = Symbol("AAPL", {"type": "stock", "country": "united states", "query": "qq"})
+    assert s.name == "AAPL"
+    assert s.type == "stock"
+    assert s.country == "united states"
+    assert s.query == "qq"
+
+
+def test_initiator_with_defaults():
+    s = Symbol("AAPL", {})
+    assert s.type == "stock"
+    assert s.country == "united states"
+    assert s.query == "AAPL"
