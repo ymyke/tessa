eb68fb5969961ccf05153922cce38586b997331d
ymyke <ymyke@users.noreply.github.com>
2022-09-11 10:25:08 +0200

    Add symbol-related names

--- tessa/__init__.py
+++ tessa/__init__.py
@@ -2,3 +2,4 @@ __version__ = "0.3.3"
 
 from .price import price_history, price_point, price_point_strict, price_latest
 from .search import search
+from .symbol import Symbol, ExtendedSymbol, SymbolCollection
