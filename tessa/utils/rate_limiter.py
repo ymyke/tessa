7b931276435bef0c4d8ff3efab434907dd627e0e
ymyke <ymyke@users.noreply.github.com>
2022-09-21 09:43:22 +0200

    Remove investpy, add yahoofinance in rate limiter

--- tessa/utils/rate_limiter.py
+++ tessa/utils/rate_limiter.py
@@ -1,9 +1,9 @@
 """Rate Limiter -- makes sure we don't hit the APIs too often by making sure a minimum
 amount of time has elapsed between calls to an API.
 
-The goal is to never run into errors in the first place, b/c at least investing.com
-takes substantial time until it allow-lists a blocked IP address again. That is also why
-we can't use a library such das Tenacity here.
+The goal is to never run into errors in the first place, b/c some sites take substantial
+time until it allow-lists a blocked IP address again. That is also why we can't use a
+library such das Tenacity here.
 """
 
 import copy
@@ -14,14 +14,14 @@ guards = {}
 
 original_guards = {
     # `reset_guards` will add `last_call` to each guard.
-    "investing_search": {
-        "wait_seconds": 1,
-    },
-    "investing": {
-        "wait_seconds": 2,
-    },
-    "crypto": {
+    "crypto": { 
+        # FIXME This should be coingecko to make more sense -- can maybe be improved if
+        # the type_ attribute of a symbol becomes more of a namespace at some point in
+        # the future
         "wait_seconds": 2.5,
+    },
+    "yahoofinance": {
+        "wait_seconds": 0.02,
     },
 }
 
@@ -43,7 +43,7 @@ def reset_guards() -> None:
 def rate_limit(type_: str) -> None:
     """Do the actual rate limiting."""
     # This is necessary because the price functions simply send the `type_`:
-    which_guard = type_ if type_ in ["crypto", "investing_search"] else "investing"
+    which_guard = type_ if type_ in ["crypto"] else "yahoofinance"
     guard = guards[which_guard]
     diff = (pendulum.now() - guard["last_call"]).total_seconds()
     if diff < guard["wait_seconds"]:
