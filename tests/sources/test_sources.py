f52555f2ff0c73cbcef8bb87d2de0196800492ca
ymyke <ymyke@users.noreply.github.com>
2023-04-03 10:25:21 +0200

    Add and use `get_price_history_bruteforcefully`, including tests

--- tests/sources/test_sources.py
+++ tests/sources/test_sources.py
@@ -2,9 +2,11 @@
 
 # pylint: disable=missing-docstring
 
+import warnings
 import pytest
-from tessa import sources
-from tessa.sources import rate_limiter
+import requests
+from tessa.sources import rate_limiter, Source, sources
+from tessa.price import PriceHistory
 
 
 def test_get_source():
@@ -29,3 +31,57 @@ def test_resetting_rate_limiters():
     assert sources.get_source("yahoo").rate_limiter.count_limited_calls == 7
     sources.reset_rate_limiters()
     assert sources.get_source("yahoo").rate_limiter.count_limited_calls == 0
+
+
+# ----- get_price_history_bruteforcefully tests -----
+
+
+def test_get_price_history_bruteforcefully_success(mocker):
+    def mock_get_price_history_success(*_, **__) -> PriceHistory:
+        return PriceHistory(df=None, currency=None)
+
+    mocker.patch("warnings.warn")
+    source = Source(
+        get_price_history=mock_get_price_history_success,
+        get_search_results=None,
+        rate_limiter=None,
+    )
+    source.get_price_history_bruteforcefully("BTC")
+    warnings.warn.assert_not_called()
+
+
+def test_get_price_history_bruteforcefully_exceeds_max_tries(mocker):
+    def mock_get_price_history_retryable_error(*_, **__) -> PriceHistory:
+        response = requests.Response()
+        response.status_code = 504
+        raise requests.HTTPError(response=response)
+
+    mocker.patch("warnings.warn")
+    source = Source(
+        get_price_history=mock_get_price_history_retryable_error,
+        get_search_results=None,
+        rate_limiter=None,
+    )
+    with pytest.raises(
+        ConnectionError, match="Cannot access source, even after 100 tries"
+    ):
+        source.get_price_history_bruteforcefully("BTC")
+    assert warnings.warn.call_count == 100
+
+
+def test_get_price_history_bruteforcefully_non_retriable_error(mocker):
+    def mock_get_price_history_non_retriable_error(*_, **__) -> PriceHistory:
+        response = requests.Response()
+        response.status_code = 400
+        raise requests.HTTPError(response=response)
+
+    mocker.patch("warnings.warn")
+    source = Source(
+        get_price_history=mock_get_price_history_non_retriable_error,
+        get_search_results=None,
+        rate_limiter=None,
+    )
+    with pytest.raises(requests.HTTPError) as exc_info:
+        source.get_price_history_bruteforcefully("BTC")
+    assert exc_info.value.response.status_code == 400
+    warnings.warn.assert_not_called()
