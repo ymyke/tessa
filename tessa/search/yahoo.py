49b265a47b11d5b2efc6f8bb70c3157e1c8cadcb
ymyke <michael.naef@gmail.com>
2025-03-13 12:55:04 +0100

    Use warning instead of simple print

--- tessa/search/yahoo.py
+++ tessa/search/yahoo.py
@@ -1,10 +1,12 @@
 """Yahoo Finance search"""
 
+import warnings
 from ..search import SearchResult
 
 
 def yahoo_search(query: str) -> SearchResult:
-    print(
-        "\nWARNING: Yahoo Finance search is not implemented due to their search result page changing to being JavaScript-based. Returning empty search result for Yahoo Finance.\n"
+    warnings.warn(
+        "Yahoo Finance search is not implemented due to their search result page changing to being JavaScript-based. Returning empty search result for Yahoo Finance.",
+        UserWarning,
     )
     return SearchResult(query, [])
